542b6f41c25e48d81a7e882be6b4c6e43f2c2cf6
Nalin Joshi <nalinjoshi629@gmail.com>
2026-05-01 13:15:56 +0530

    Implement bubble sort and ASCII code printer in C; refactor main function for clarity

--- unit1(array).c
+++ unit1(array).c
@@ -1,48 +1,116 @@
 #include <stdio.h>
-
-int main(void)
-{
-    int N;
-
-    printf("Enter dimension N: ");
-    if (scanf("%d", &N) != 1 || N <= 0) {
-        return 1;
+int main() {
+    int arr[10];
+    printf("Enter 10 integers:\n");
+    for(int i = 0; i < 10; i++) {
+        scanf("%d", &arr[i]);
     }
-
-    int a[N][N];
-    int b[N][N];
-    int c[N][N];
-
-    printf("Enter elements of first matrix:\n");
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            scanf("%d", &a[i][j]);
-        }
-    }
-
-    printf("Enter elements of second matrix:\n");
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            scanf("%d", &b[i][j]);
-        }
-    }
-
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            c[i][j] = 0;
-            for (int k = 0; k < N; k++) {
-                c[i][j] += a[i][k] * b[k][j];
+    // Bubble sort in ascending order
+    for(int i = 0; i < 9; i++) {
+        for(int j = 0; j < 9 - i; j++) {
+            if(arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
-
-    printf("Product matrix:\n");
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            printf("%d ", c[i][j]);
-        }
-        printf("\n");
+    printf("Sorted array in ascending order:\n");
+    for(int i = 0; i < 10; i++) {
+        printf("%d ", arr[i]);
     }
-
+    printf("\n");
     return 0;
 }
+// int main(){
+//     // int arr[] = {2,3,4,5,6,7};    
+//     int n = 6;
+
+//     for(int i = n; i >= 1; i--){
+//         for(int j = 0; j< i; j++){
+//             // printf("%d", arr[j]);
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+//----------------------------------------------------------------------------------------------
+// question 2: linear search in an array
+// int main(){
+//      int arr[20] = {21,32,43,54,65,23,43,54,65,76,87,98,12,23,34,45,56,67,78,89};
+//     int target;
+//     int found = 0;
+//     int position = -1;
+    
+//     printf("Enter the integer to search: ");
+
+//     if(scanf("%d", &target) == 0){
+//         return 1;
+//     }
+//     for (int i = 0; i < 20; i++){
+//         if(arr[i] == target){
+//             found = 1;
+//             position = i;
+//             break;
+//         }
+//     }
+//     if(found){
+//         printf("Integer %d found at position %d\n", target, position);
+//     } else {
+//         printf("Integer %d not found in the array\n", target);
+//     }
+//     return 0;
+// }
+
+
+
+
+//----------------------------------------------------------------------------------------------
+//question 3: matrix multiplication 
+// # define N 3
+// int main() {
+//     int A[N][N];
+//     int B[N][N];
+//     int C[N][N];
+    
+//     printf("Enter your first element: \n");
+//     for(int i = 0; i<N; i++){
+//         for(int j = 0; j<N; j++){
+//             scanf("%d", &A[i][j]);
+//         }
+//     }
+//     printf("Enter your second matrix: \n");
+//         for(int i = 0; i<N; i++){
+//             for(int j = 0; j<N; j++){
+//                 scanf("%d", &B[i][j]);
+//             }
+//     }
+//     for(int i = 0; i<N; i++){
+//         for(int j = 0; j<N; j++){
+//                 C[i][j] = 0;
+//             }
+//     }
+        
+//     for(int i = 0; i<N; i++){
+//         for(int j = 0; j<N; j++){
+//             C[i][j] += A[i][j] * B[i][j];
+//         }
+//     }
+    
+//     for(int i = 0; i<N; i++){
+//         for(int j = 0; j<N; j++){
+//             C[i][j] += A[i][j] * B[i][j];
+//         }
+//     }
+//     printf("Result: ");
+//     for(int i = 0; i<N; i++){
+//         for(int j = 0; j<N; j++){
+//             printf("%d ", C[i][j]);
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
--- unit3(strings).c
+++ unit3(strings).c
@@ -1,18 +1,84 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main()
+// ASCII code printer for a string in C
+int main(void)
 {
-    char str[100];
-    printf("Enter a string: ");
-    scanf("%s", str);
-    
-    for(int i = 0; str[i] != '\0'; i++)
-    {
-        if(str[i] >= 'a' && str[i] <= 'z')
-        {
-            str[i] = str[i] - 32;
+    char name[256];
+
+    printf("Enter your name: ");
+    if (fgets(name, sizeof(name), stdin) == NULL) {
+        return 1;
+    }
+
+    size_t len = strlen(name);
+    if (len > 0 && name[len - 1] == '\n') {
+        name[len - 1] = '\0';
+        len--;
+    }
+
+    printf("ASCII codes for '%s':\n", name);
+    for (size_t i = 0; i < len; i++) {
+        printf("%d", (unsigned char)name[i]);
+        if (i + 1 < len) {
+            printf(" ");
         }
     }
-    printf("Modified string: %s", str);
+    printf("\n");
+
     return 0;
 }
+
+// Convert lowercase letters to uppercase in a string in C
+// int main()
+// {
+//     char str[100];
+//     printf("Enter a string: ");
+//     scanf("%s", str);
+    
+//     for(int i = 0; str[i] != '\0'; i++)
+//     {
+//         if(str[i] >= 'a' && str[i] <= 'z')
+//         {
+//             str[i] = str[i] - 32;
+//         }
+//     }
+//     printf("Modified string: %s", str);
+//     return 0;
+// }
+// int main(void)
+// {
+//     size_t capacity = 256;
+//     char *source = malloc(capacity);
+//     if (source == NULL) {
+//         perror("malloc");
+//         return 1;
+//     }
+
+//     printf("Enter a string: ");
+//     if (fgets(source, capacity, stdin) == NULL) {
+//         free(source);
+//         return 1;
+//     }
+
+//     size_t len = strlen(source);
+//     if (len > 0 && source[len - 1] == '\n') {
+//         source[len - 1] = '\0';
+//         len--;
+//     }
+
+//     char *copy = malloc(len + 1);
+//     if (copy == NULL) {
+//         perror("malloc");
+//         free(source);
+//         return 1;
+//     }
+
+//     strcpy(copy, source);
+//     printf("Copied string: %s\n", copy);
+
+//     free(copy);
+//     free(source);
+//     return 0;
+// }
