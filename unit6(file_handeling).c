663f2984010388b55feeae28b0e5663ed3c7426b
Nalin Joshi <nalinjoshi629@gmail.com>
2026-05-05 14:18:16 +0530

    Refactor file handling: implement input file reading and separate numbers into ODD and EVEN files

--- unit6(file_handeling).c
+++ unit6(file_handeling).c
@@ -1,26 +1,57 @@
 #include <stdio.h>
 #include <ctype.h>
-
 int main() {
-    FILE *ready, *copen;
-    char *filename_in = "txtfile/text.txt";
-    char *filename_out = "txtfile/newfile.txt";
-    int x;
+    FILE *input, *odd, *even;
+    int num;
     
-    ready = fopen(filename_in, "r"); 
-    copen = fopen(filename_out, "w");
+    // Open INPUT file for reading
+    input = fopen("txtfile/INPUT.txt", "r");
+    odd = fopen("txtfile/ODD.txt", "w");
     
-    while ((x = fgetc(ready)) != EOF) {//end of file
-        fputc(toupper(x), copen);//toupper converting to uppercase
+    // Open EVEN file for writing
+    even = fopen("txtfile/EVEN.txt", "w");
+
+    // Read numbers from INPUT and distribute to ODD/EVEN
+    while (fscanf(input, "%d", &num) != EOF) {
+        if (num % 2 == 0) {
+            fprintf(even, "%d\n", num);
+        } else {
+            fprintf(odd, "%d\n", num);
+        }
     }
     
-    fclose(ready);
-    fclose(copen);
+    // Close all files
+    fclose(input);
+    fclose(odd);
+    fclose(even);
     
-    printf("File conversion completed successfully!\n");
+    printf("Numbers separated successfully!\n");
+    printf("Odd numbers written to ODD file\n");
+    printf("Even numbers written to EVEN file\n");
     
     return 0;
 }
+
+// int main() {
+//     FILE *ready, *copen;
+//     char *filename_in = "txtfile/text.txt";
+//     char *filename_out = "txtfile/newfile.txt";
+//     int x;
+    
+//     ready = fopen(filename_in, "r"); 
+//     copen = fopen(filename_out, "w");
+    
+//     while ((x = fgetc(ready)) != EOF) {//end of file
+//         fputc(toupper(x), copen);//toupper converting to uppercase
+//     }
+    
+//     fclose(ready);
+//     fclose(copen);
+    
+//     printf("File conversion completed successfully!\n");
+    
+//     return 0;
+// }
 
 // int main()
 // {
